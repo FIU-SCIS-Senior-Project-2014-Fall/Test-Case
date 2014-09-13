7316c8c8936f8f37ad64eff8f1467d96be7be7f4
Justin Phillips <me@justinphillips.co>
2014-09-12 23:23:06 -0400

    Semi-first draft of layout and more dependencies
    Laid out a foundation for a potential layout draft, also added more
    dependencies from Bower.

--- code/mockup/app/scripts/controllers/main.js
+++ code/mockup/app/scripts/controllers/main.js
@@ -9,9 +9,9 @@
  */
 angular.module('initProjApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.testArch = [
+      { "id" : 0, "name" : 'Test Suite 1', "type" : "suite", "size" : "-lg" },
+      { "id" : 1, "name" : 'Case 1', "type" : "case", "parent" : 0, "size" : "" },
+      { "id" : 2, "name" : 'Step 1', "type" : "step", "parent" : 1, "size" : "-sm" }
     ];
   });
