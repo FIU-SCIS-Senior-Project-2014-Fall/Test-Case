6ec2a4dc869fe100b5d4a18faa0de4ff69166114
Justin Phillips <jphil075@fiu.edu>
2014-11-10 11:34:16 -0500

    added back attachements and metrics

--- code/mockup/app/scripts/controllers/workspace.js
+++ code/mockup/app/scripts/controllers/workspace.js
@@ -513,6 +513,56 @@ angular.module('initProjApp').controller('WorkspaceCtrl', function ($scope, stor
 		return size;
 	};
 
+	$scope.getMetrics = function(index) {
+		$("#myModal").find(".modal-title").html($scope.suite.children[index].name + " Metrics");
+		$("#myModal").find(".modal-body").html('<canvas id="myChart" width="400" height="400"></canvas>');
+		$("#myModal").modal('show');
+
+		var ctx = $("#myChart").get(0).getContext("2d"); 
+ 
+        var data = [
+            {
+                value: 25,
+                color:"#F7464A",
+                highlight: "#FF5A5E",
+                label: "Product Failure",
+                labelColor : 'Black',
+                labelFontSize : '16'
+            },
+            {
+                value: 100,
+                color: "#46BFBD",
+                highlight: "#5AD3D1",
+                label: "Test Success"
+            },
+            {
+                value: 15,
+                color: "#FDB45C",
+                highlight: "#FFC870",
+                label: "Test Failure"
+            }
+        ];
+
+        var options = { 
+          legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+        };
+ 
+        var chart = new Chart(ctx).Doughnut(data, options);
+        //var legend = chart.generateLegend();
+        //$(".modal-body").append(legend);
+	}
+
+	$scope.getAttachements = function(index) {
+		$("#myModal").find(".modal-title").html($scope.suite.children[index].name + " Attachement(s)");
+		$("#myModal").find(".modal-body").html(
+			'<div class="form-group">'+
+		    	'<label for="exampleInputFile">File Upload</label>'+
+		    	'<input type="file" id="exampleInputFile">'+
+		    	'<p class="help-block">Upload attachments here.</p>'+
+		  	'</div>');
+		$("#myModal").modal('show');
+	}
+
 	/* utilities */
 
 	Array.prototype.clone = function() {
