0ab997962c794a56f2c9ff9ea6994645604fac7f
Justin Phillips <jphil075@fiu.edu>
2014-11-10 18:09:03 -0500

    Working build of TFS API client.

--- code/Backend/TFSTest/Program.cs
+++ code/Backend/TFSTest/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using vTeam.TfsApi;
@@ -14,18 +16,21 @@ namespace TFSTest
             string Uri = "http://tc-dev.cis.fiu.edu:8080/tfs/";
             string Collection = "DefaultCollection";
             string Project = "HelloWorld";
+            ICredentials crds = new NetworkCredential("TFS", "test123");
 
             try
             {
-                var mtmManager = new MtmManager(Uri, Collection, Project);
-                var tfsManager = new TfsManager(Uri, Collection, Project);
+                //var mtmManager = new MtmManager(Uri, Collection, Project);
+                var tfsManager = new TfsManager(Uri, Collection, Project, crds);
 
                 var testSuite = tfsManager.CreateWorkItem(tfsManager.GetWorkItemType("Test Suite"), "FIU Hello Suite",
                     new Dictionary<WorkItemField, string>
                 {
-                    {WorkItemField.Stories, "ULV1511"},
-                    {WorkItemField.Author1, "Justin Phillips"}
                 });
+
+                var testCase = tfsManager.CreateWorkItem(tfsManager.GetWorkItemType("Test Case"), "HelloHelloHello");
+
+                tfsManager.LinkWorkItems(WorkItemLinkEndType.TestCase, testSuite, testCase.Id);
             }
             catch(Exception e)
             {
