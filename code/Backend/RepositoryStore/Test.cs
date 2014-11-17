26ff5918e8cdb08b3eb245176254977de658526c
Justin Phillips <jphil075@fiu.edu>
2014-11-17 11:48:16 -0500

    Changes to UML, added GIT lib, added web.config to app

---
+++ code/Backend/RepositoryStore/Test.cs
@@ -0,0 +1,38 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryStore
+{
+    public class Test
+    {
+
+        public void Connect(string url)
+        {
+            // Location on the disk where the local repository should be cloned
+            string workingDirectory = "dddd";
+
+            // Perform the initial clone
+            if(!Repository.IsValid(workingDirectory))
+                workingDirectory = Repository.Clone(url, workingDirectory);
+
+            using (var repo = new Repository(workingDirectory))
+            {
+                Commit commit = repo.Head.Tip;
+                foreach (TreeEntry treeEntry in commit.Tree)
+                {
+                    Console.WriteLine("Path:{0} || Type:{1} || Target:{2}", treeEntry.Path, treeEntry.TargetType, treeEntry.Target);
+                    if(treeEntry.TargetType == TreeEntryTargetType.Tree)
+                    {
+                        foreach(TreeEntry treeChild in  (Tree)treeEntry.Target)
+                            Console.WriteLine("        Path:{0} || Type:{1} || Target:{2}", treeChild.Path, treeChild.TargetType, treeChild.Target);
+                    }
+                        
+                }
+            }
+        }
+    }
+}
--- code/Backend/TFSTest/Program.cs
+++ code/Backend/TFSTest/Program.cs
@@ -1,31 +1,45 @@
-﻿using System;
+﻿using RepositoryStore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Security;
 using System.Text;
 using System.Threading.Tasks;
-using vTeam.TfsApi;
 
 namespace TFSTest
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            /*
             string Uri = "http://tc-dev.cis.fiu.edu:8080/tfs/";
             string Collection = "DefaultCollection";
             string Project = "HelloWorld";
+            ICredentials crds = new NetworkCredential("TFS", "test123");   
+            ITestManagementTeamProject project = GetProject(Uri + Collection, Project, crds);
+            */
+
+            Test repoTest = new Test();
+            repoTest.Connect(@"https://github.com/FIU-SCIS-Senior-Project-2014-Fall/Test-Case.git");
+
+            
+
+            /*
             ICredentials crds = new NetworkCredential("TFS", "test123");
 
             try
             {
                 //var mtmManager = new MtmManager(Uri, Collection, Project);
+
                 var tfsManager = new TfsManager(Uri, Collection, Project, crds);
 
                 var testSuite = tfsManager.CreateWorkItem(tfsManager.GetWorkItemType("Test Suite"), "FIU Hello Suite",
                     new Dictionary<WorkItemField, string>
                 {
+                    {WorkItemField.}
                 });
 
                 var testCase = tfsManager.CreateWorkItem(tfsManager.GetWorkItemType("Test Case"), "HelloHelloHello");
@@ -35,7 +49,7 @@ namespace TFSTest
             catch(Exception e)
             {
                 Console.WriteLine(e.InnerException.ToString());
-            }
+            }*/
             Console.ReadLine();
             
         }
